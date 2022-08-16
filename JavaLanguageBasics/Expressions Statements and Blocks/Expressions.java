ee5a75aa255dcde067f6894f8e778f2c48ffb712
madan3242 <madangowda3242@gmail.com>
2022-08-16 10:41:43 +0530

    Expressions, Statements and Blocks

---
+++ JavaLanguageBasics/Expressions Statements and Blocks/Expressions.java
@@ -0,0 +1,26 @@
+/**
+ * Expressions
+ */
+public class Expressions {
+
+    public static void main(String[] args) {
+        int cadence = 0;
+        anArray[0] = 100;
+        System.out.println("Element 1 at index 0: " + anArray[0]);
+
+        int result = 1 + 2; // result is now 3
+        if (value1 == value2)
+            System.out.println("value1 == value2");
+
+        // 1 * 2 * 3
+
+        // x + y / 100;
+
+        // (x + y)/100;
+
+        // x + y / 100   ambiguous
+
+        // x + (y / 100)  unambiguous, recommended
+
+    }
+}
---
+++ JavaLanguageBasics/Expressions Statements and Blocks/Statements.java
@@ -0,0 +1,22 @@
+class Bicycle{
+
+}
+public class Statements {
+    public static void main(String[] args) {
+        // declaration statement
+        double aValue = 8933.234;
+
+        // assignment statement
+        aValue = 8933.234;
+
+        // increment statement
+        aValue++;
+
+        // method invocation statement
+        System.out.println("Hello World!");
+
+        // object creation statement
+        Bicycle myBike = new Bicycle();
+
+    }
+}
