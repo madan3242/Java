1f95774be71c2565449da1065b241610add879e8
madan3242 <madangowda3242@gmail.com>
2022-08-18 11:40:09 +0530

    even odd numbers

--- Java/Iterative Statements/OneToTenEvenNumbers.java
+++ Java/Iterative Statements/OneToTenEvenNumbers.java
@@ -1,9 +1,11 @@
 //Program to print even numbers form 1 to 10
 public class OneToTenEvenNumbers {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
+        int i = 1;
+        while (i <= 10) {
             if (i % 2 == 0) {
                 System.out.println(i);
+                i++;
             }
         }
     }
--- Java/Iterative Statements/OneToTenOddNumbers.java
+++ Java/Iterative Statements/OneToTenOddNumbers.java
@@ -1,9 +1,11 @@
 //Program to print even numbers form 1 to 10
 public class OneToTenOddNumbers {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            if (i % 2 != 0) {
+        int i = 1;
+        while (i <= 10) {
+            if (i % 2 == 0) {
                 System.out.println(i);
+                i++;
             }
         }
     }
