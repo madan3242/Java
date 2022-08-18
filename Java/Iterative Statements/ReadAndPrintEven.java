385a5c26f8eae8bc371dbddd4860caffbeceb353
madan3242 <madangowda3242@gmail.com>
2022-08-18 12:39:11 +0530

    Even And Odd

--- Java/Iterative Statements/ReadAndPrintEven.java
+++ Java/Iterative Statements/ReadAndPrintEven.java
@@ -24,6 +24,12 @@ public class ReadAndPrintEven {
                 System.out.println(arr[i]);
             }
         }
+        System.out.println("Odd numbers are :");
+        for(int i = 0; i < arr.length; i++){
+            if (arr[i] % 2 != 0) {
+                System.out.println(arr[i]);
+            }
+        }
 
     }
 }
