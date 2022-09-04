3bac260c89e37f94bb2f0c24cec3f91bb361c53f
madan3242 <madangowda3242@gmail.com>
2022-09-04 19:39:45 +0530

    string

--- JavaLanguageBasics/Strings/ConcateString.java
+++ JavaLanguageBasics/Strings/ConcateString.java
@@ -1,26 +1,26 @@
 public class ConcateString {
     public static void main(String[] args) {
-        string1.concat(string2); 
+        // string1.concat(string2); 
 
-        "My name is ".concat("Rumplestiltskin");
+        // "My name is ".concat("Rumplestiltskin");
 
-        "Hello," + " world" + "!"
+        // "Hello," + " world" + "!"
 
-        "Hello, world!"
+        // "Hello, world!"
 
-        String string1 = "saw I was ";
-        System.out.println("Dot " + string1 + "Tod");
+        // String string1 = "saw I was ";
+        // System.out.println("Dot " + string1 + "Tod");
 
-        String quote = 
-            "Now is the time for all good " +
-            "men to come to the aid of their country.";
+        // String quote = 
+        //     "Now is the time for all good " +
+        //     "men to come to the aid of their country.";
 
-            String html = """
-                <html>
-                    <body>
-                        <p>Hello, world</p>
-                    </body>
-                </html>
-                """;
+        //     String html = """
+        //         <html>
+        //             <body>
+        //                 <p>Hello, world</p>
+        //             </body>
+        //         </html>
+        //         """;
     }
 }
---
+++ JavaLanguageBasics/Strings/FormatString.java
@@ -0,0 +1,19 @@
+public class FormatString {
+    public static void main(String[] args) {
+        // System.out.printf("The value of the float " +
+        //           "variable is %f, while " +
+        //           "the value of the " + 
+        //           "integer variable is %d, " +
+        //           "and the string is %s", 
+        //           floatVar, intVar, stringVar); 
+                  
+        // String fs;
+        // fs = String.format("The value of the float " +
+        //                      "variable is %f, while " +
+        //                      "the value of the " + 
+        //                      "integer variable is %d, " +
+        //                      " and the string is %s",
+        //                     floatVar, intVar, stringVar);
+        // System.out.println(fs);
+    }
+}
